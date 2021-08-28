7c5b61c5a67e03fd17fccc0222792c13a03ac3b2
sangam11a <73978251+sangam11a@users.noreply.github.com>
2021-08-28 09:16:22 +0545

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+/**
+ * @author Amir Sanni <amirsanni@gmail.com>
+ * @date 23-Dec-2016
+ */
+
+'use strict';
+
+
+const appRoot = 'http://localhost:8888/';
+const wsUrl = 'ws://localhost:8080';//use wss://localhost:8080/comm for secured connection
+const spinnerClass = 'fa fa-spinner faa-spin animated';
